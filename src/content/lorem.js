f9fb44a159911074f3ed2601ed9e0309f2ffb6f5
eimaj <jamie@llen.ca>
2018-07-22 14:32:17 -0400

    Creates two placeholder content components that are rendered into Content.

---
+++ src/content/lorem.js
@@ -0,0 +1,46 @@
+export default `# Lorem ipsum dolor
+
+Quoniam, si dis placet, ab Epicuro loqui discimus. Praeteritis, inquit, gaudeo. Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. Et quae per vim oblatum stuprum volontaria morte lueret inventa est et qui interficeret filiam, ne stupraretur. Sed in rebus apertissimis nimium longi sumus. Omnia contraria, quos etiam insanos esse vultis.
+
+## Quo plebiscito decreta a senatu est
+
+Et harum quidem rerum facilis est et expedita distinctio. At ille non pertimuit saneque fidenter: Istis quidem ipsis verbis, inquit; Nonne videmus quanta perturbatio rerum omnium consequatur, quanta confusio? Nos commodius agimus. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quod quidem nobis non saepe contingit.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Equidem e Cn. Fortasse id optimum, sed ubi illud: Plus semper voluptatis? In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Magno hic ingenio, sed res se tamen sic habet, ut nimis imperiosi philosophi sit vetare meminisse. Hic ego: Pomponius quidem, inquam, noster iocari videtur, et fortasse suo iure. Duo Reges: constructio interrete.
+
+## Praeteritis, inquit, gaudeo. Fieri, inquam
+
+Quo plebiscito decreta a senatu est consuli quaestio Cn. Facit igitur Lucius noster prudenter, qui audire de summo bono potissimum velit;
+
+Et harum quidem rerum facilis est et expedita distinctio. At ille non pertimuit saneque fidenter: Istis quidem ipsis verbis, inquit; Nonne videmus quanta perturbatio rerum omnium consequatur, quanta confusio? Nos commodius agimus. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quod quidem nobis non saepe contingit.
+
+## Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Equidem e Cn. Fortasse id optimum, sed ubi illud: Plus semper voluptatis? In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Magno hic ingenio, sed res se tamen sic habet, ut nimis imperiosi philosophi sit vetare meminisse. Hic ego: Pomponius quidem, inquam, noster iocari videtur, et fortasse suo iure. Duo Reges: constructio interrete.
+
+* Quoniam, si dis placet, ab
+* Epicuro loqu discimus.
+* Praeteritis, inquit, gaudeo. Fieri, inquam,
+* Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias.
+* Et quae per vim oblatum stuprum volontaria morte lueret
+* Dinventa est et qui interficeret filiam, ne stupraretur.
+* Sed in rebus apertissimis nimium longi sumus.
+* Omnia contraria, quos etiam insanos esse vultis.
+
+Quo plebiscito decreta a senatu est consuli quaestio Cn. Facit igitur Lucius noster prudenter, qui audire de summo bono potissimum velit;
+
+### Et harum quidem rerum facilis est et expedita distinctio. At ille non pertimuit saneque
+
+Ffidenter: Istis quidem ipsis verbis, inquit; Nonne videmus quanta perturbatio rerum omnium consequatur, quanta confusio? Nos commodius agimus. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quod quidem nobis non saepe contingit.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Equidem e Cn. Fortasse id optimum, sed ubi illud: Plus semper voluptatis? In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Magno hic ingenio, sed res se tamen sic habet, ut nimis imperiosi philosophi sit vetare meminisse. Hic ego: Pomponius quidem, inquam, noster iocari videtur, et fortasse suo iure. Duo Reges: constructio interrete.
+
+## Epicuro loqui discimus.
+
+Praeteritis, inquit, gaudeo. Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. Et quae per vim oblatum stuprum volontaria morte lueret inventa est et qui interficeret filiam, ne stupraretur. Sed in rebus apertissimis nimium longi sumus. Omnia contraria, quos etiam insanos esse vultis.
+
+
+Quo plebiscito decreta a senatu est consuli quaestio Cn. Facit igitur Lucius noster prudenter, qui audire de summo bono potissimum velit;
+
+Et harum quidem rerum facilis est et expedita distinctio. At ille non pertimuit saneque fidenter: Istis quidem ipsis verbis, inquit; Nonne videmus quanta perturbatio rerum omnium consequatur, quanta confusio? Nos commodius agimus. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quod quidem nobis non saepe contingit.
+`
---
+++ src/content/loremTwo.js
@@ -0,0 +1,19 @@
+export default `# Number 2
+
+Quoniam, si dis placet, ab Epicuro loqui discimus. Praeteritis, inquit, gaudeo. Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. Et quae per vim oblatum stuprum volontaria morte lueret inventa est et qui interficeret filiam, ne stupraretur. Sed in rebus apertissimis nimium longi sumus. Omnia contraria, quos etiam insanos esse vultis.
+
+## Quo plebiscito decreta a senatu est
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Equidem e Cn. Fortasse id optimum, sed ubi illud: Plus semper voluptatis? In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Magno hic ingenio, sed res se tamen sic habet, ut nimis imperiosi philosophi sit vetare meminisse. Hic ego: Pomponius quidem, inquam, noster iocari videtur, et fortasse suo iure. Duo Reges: constructio interrete.
+
+* Quoniam, si dis placet, ab
+* Epicuro loqu discimus.
+* Praeteritis, inquit, gaudeo. Fieri, inquam,
+* Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias.
+* Et quae per vim oblatum stuprum volontaria morte lueret
+* Dinventa est et qui interficeret filiam, ne stupraretur.
+* Sed in rebus apertissimis nimium longi sumus.
+* Omnia contraria, quos etiam insanos esse vultis.
+
+Quo plebiscito decreta a senatu est consuli quaestio Cn. Facit igitur Lucius noster prudenter, qui audire de summo bono potissimum velit;
+`
