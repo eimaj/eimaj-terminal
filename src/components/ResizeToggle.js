95ec2d9fe5cc3136c3655cf84065ef45ea6d1156
eimaj <jamie@llen.ca>
2018-07-28 13:21:59 -0400

    Creates ResizeToggle component and styles

---
+++ src/components/ResizeToggle.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../styles/ResizeToggle.css';
+
+const ResizeToggle = ({ toggleContentSize, maxContentSize }) => {
+
+  const maximizeTerminal = (e) => {
+    e.preventDefault();
+    return toggleContentSize(24);
+  };
+
+  const minimizeTerminal = (e) => {
+    e.preventDefault();
+    return toggleContentSize(maxContentSize - 24);
+  };
+
+  return (
+    <div className="ResizeToggle">
+      <a
+        className="ResizeToggle__link ResizeToggle__link--maximize"
+        onClick={maximizeTerminal}
+        href="#maximize"
+      >
+        <i className="ResizeToggle__icon" />
+        Maximize content
+      </a>
+
+      <a
+        className="ResizeToggle__link ResizeToggle__link--minimize"
+        onClick={minimizeTerminal}
+        href="#minimize"
+      >
+        <i className="ResizeToggle__icon" />
+        Maximize terminal
+      </a>
+    </div>
+  )
+};
+
+ResizeToggle.propTypes = {
+  toggleContentSize: PropTypes.func.isRequired,
+  maxContentSize: PropTypes.number.isRequired,
+};
+
+export default ResizeToggle;
