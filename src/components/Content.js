2fe89a22d83a5a2b48bb36f9701f177d1e347c01
eimaj <jamie@llen.ca>
2018-07-28 13:27:25 -0400

    Updates Content with styles, a Header and social Icons

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Markdown from 'markdown-to-jsx';
-import markdownContent from '../content/index';
 
+import Header from './Header';
+import Icons from './Icons';
+
+import pages from '../content/index';
 import '../styles/Content.css';
 
-const Content = ({ content }) => {
-  console.log(content);
+const Content = ({ page }) => {
+  const { classNames, content } = pages[page];
 
   return (
-    <div className="Content">
-      <Markdown>
-        {markdownContent[content]}
-      </Markdown>
+    <div className={['Content', classNames].join(' ')}>
+      <Header />
+      <div className="Content__box">
+        <Markdown>
+          {content}
+        </Markdown>
+        <hr />
+        <Icons />
+      </div>
     </div>
   )
 };
 
 Content.defaultProps = {
-  content: '',
+  page: '',
 };
 
 Content.propTypes = {
-  content: PropTypes.string,
+  page: PropTypes.string,
 };
 
 export default Content;
