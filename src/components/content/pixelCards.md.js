5a29fff5e747c79d200c6c4dcef1d8b77e3556fe
eimaj <jamie@llen.ca>
2018-07-29 14:48:27 -0400

    Updates the pixel page.

--- src/components/content/pixelCards.md.js
+++ src/components/content/pixelCards.md.js
@@ -4,8 +4,12 @@ export default `# Pixel Playing Cards
 
 A couple years ago I designed a deck of cards.
 
-I had a couple decks printed and recieved a lot of encouragement from friends and family to start a funding campaign. Life got complicated and it never got off the ground. Some day I will, but for now it is just a labour of love.
+I had a couple printed (one read and one blue) and recieved a lot of encouragement from friends and family to start a funding campaign. Then life got complicated.
 
-![Ten of Diamonds and Jack of Spades](${pixelPreview})
+Some day I will try to fund and sell them, but for now it is just a labour of love.
 
-Feel free to reach out if you want to know more.`;
+---
+
+Feel free to reach out if you want to know more.
+
+![Ten of Diamonds and Jack of Spades](${pixelPreview})`;
