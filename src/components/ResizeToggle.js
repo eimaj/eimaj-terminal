34619f200ba911a832f8ee2899c8c6974c83f224
eimaj <jamie@llen.ca>
2018-07-29 08:40:30 -0400

    Implements splitTerminalAndContent action with new link in ResizeToggle

--- src/components/ResizeToggle.js
+++ src/components/ResizeToggle.js
@@ -23,20 +23,35 @@ const ResizeToggle = ({ toggleContentSize, maxContentSize }) => {
   return (
     <div className="ResizeToggle">
       <a
-        className="ResizeToggle__link ResizeToggle__link--maximize"
+        className="ResizeToggle__link"
         onClick={maximizeTerminal}
         href="#maximize"
+        alt="Maximize content"
+        title="Maximize content"
       >
-        <i className="ResizeToggle__icon" />
+        <i className="ResizeToggle__icon ResizeToggle__icon--maximize" />
         Maximize content
       </a>
 
       <a
-        className="ResizeToggle__link ResizeToggle__link--minimize"
+        className="ResizeToggle__link"
+        onClick={splitTerminalAndContent}
+        href="#split"
+        alt="Split terminal and content"
+        title="Split terminal and content"
+      >
+        <i className="ResizeToggle__icon ResizeToggle__icon--split" />
+        Split terminal and content
+      </a>
+
+      <a
+        className="ResizeToggle__link"
         onClick={minimizeTerminal}
         href="#minimize"
+        alt="Minimize terminal"
+        title="Minimize terminal"
       >
-        <i className="ResizeToggle__icon" />
+        <i className="ResizeToggle__icon ResizeToggle__icon--minimize" />
         Maximize terminal
       </a>
     </div>
