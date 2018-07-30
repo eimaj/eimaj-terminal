858265ee6d83c9f1c15105ff6b856a1b9035df0c
eimaj <jamie@llen.ca>
2018-07-30 19:33:24 -0400

    Adds docs to executeCommand/help and refactors a bit.

--- src/helpers/executeCommand/help.js
+++ src/helpers/executeCommand/help.js
@@ -1,18 +1,50 @@
-const helpResponse = { message: `Eimaj CLI, v1.0. A fun way to intereact with my website.
+/**
+ * renderMessage - The help message for the `render` command.
+ *
+ * @return {String}
+ */
+const renderMessage = `render  Renders requested page content.
+        Usage:  render [page]
+        Options:
+            -h, --help    Prints help for render`;
+
+/**
+ * helpMessage - The help message for the `help` command.
+ *
+ * @return {String}
+ */
+const helpMessage = `help    Prints this helpful text.
+        Usage:  help`;
+
+/**
+ * helpResponses (export) - A compiled Object of all available command help messages.
+ *
+ * @return {Object}
+ */
+export const helpResponses = {
+  render: renderMessage,
+  help: helpMessage,
+}
+
+/**
+ * helpFlags (export) - A list of flags for commands that can trigger a help message.
+ */
+export const helpFlags = ['--help', '-h'];
+
+/**
+ * help - The method that is called when the help command it submitted.
+ *
+ * @return {Object}   Contains only { message: String }
+ */
+const help = () => {
+  return {
+    message: `Eimaj CLI, v1.0. A fun way to intereact with my website.
 
 Available commands:
 
-render  Renders requested page content.
-        Usage:  render [page]
-        Options:
-            -h, --help    Prints help for render
-
-help    Prints this helpful text.
-        Usage:  help`
-};
-
-const help = (payload = '') => {
-  return helpResponse;
+${renderMessage}
+${helpMessage}`
+  };
 };
 
 export default help;
