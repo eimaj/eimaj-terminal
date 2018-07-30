4b49919f77073af9533ff9d59379f8978d6fdfbc
eimaj <jamie@llen.ca>
2018-07-30 10:13:10 -0400

    Remove deprecated fonts css from index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './fonts/ionicons.css';
 import './styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
