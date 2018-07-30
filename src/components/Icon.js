300bba02825dbd999e87ab1d93c77fe9b71c0995
eimaj <jamie@llen.ca>
2018-07-30 10:10:43 -0400

    Remove icon font and replace with 5 svg components.

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Download from './svg/Download';
+import GitHub from './svg/GitHub';
+import Instagram from './svg/Instagram';
+import LinkedIn from './svg/LinkedIn';
+import Twitter from './svg/Twitter';
+
 import '../styles/Icon.css';
 
-const Icon = ({ name, icon, title, url }) => (
-  <li className="Icon">
-    <a
-      className={`Icon__link Icon--${name}`}
-      href={url}
-      title={title}
-      alt={title}
-      target="_"
-    >
-      <i className={`ion ion-${icon}`} />
-    </a>
-  </li>
-);
+const Icon = ({ name, title, url }) => {
+  let Svg;
+
+  switch (title) {
+    case 'Twitter':
+      Svg = Twitter;
+      break;
+    case 'Instagram':
+      Svg = Instagram;
+      break;
+    case 'LinkedIn':
+      Svg = LinkedIn;
+      break;
+    case 'GitHub':
+      Svg = GitHub;
+      break;
+    default:
+      Svg = Download;
+  }
+
+  return (
+    <li className="Icon">
+      <a
+        className={`Icon__link Icon--${name}`}
+        href={url}
+        title={title}
+        alt={title}
+        target="_"
+      >
+        <svg x="0px" y="0px"
+           width="100%" height="100%" viewBox="0 0 512 512" fill="white">
+          <Svg />
+        </svg>
+      </a>
+    </li>
+  );
+};
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
 };
-
 
 export default Icon;
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -8,35 +8,30 @@ const Icons = () => (
   <ul className="Icons">
     <Icon
       name="linkedin"
-      icon="social-linkedin"
       title="LinkedIn"
       url="https://linkedin.com/eimaj"
     />
 
     <Icon
       name="github"
-      icon="social-github"
       title="GitHub"
       url="https://github.com/eimaj"
     />
 
     <Icon
       name="twitter"
-      icon="social-twitter"
       title="Twitter"
       url="https://twitter.com/eimaj"
     />
 
     <Icon
       name="instagram"
-      icon="social-instagram"
       title="Instagram"
       url="https://instagram.com/eimaj"
     />
 
     <Icon
       name="resume"
-      icon="android-download"
       title="Download Resume as PDF"
       url="./jamie-allen_resume.pdf"
     />
