a2445b85cd313183a08e5bb2faa9808032a54372
eimaj <jamie@llen.ca>
2018-08-04 23:22:53 -0400

    Extracts Content/Terminal SplitPane resizing for re-use.

---
+++ src/helpers/resizeContent.js
@@ -0,0 +1,64 @@
+
+/**
+ * minContentSize - The minimum size the Content and Terminal component Panes can be.
+ */
+const minContentSize = 24;
+
+/**
+ * minContentValue (export) - A function that returns the minContentSize.
+ *
+ * @return {Function}
+ */
+export const minContentValue = () => minContentSize;
+
+/**
+ * splitValue (export) - A function that returns 50% to split the Content and Terminal Panes evenly.
+ *
+ * @return {Function}
+ */
+export const splitValue = () => '50%';
+
+/**
+ * minContentValue (export) - A function that returns the maximum size the content can be
+ * based on the maxContentSize passed in as a param (via <App>).
+ *
+ * @return {Function}
+ */
+export const maxContentValue = (maxContentSize) => maxContentSize - minContentSize;
+
+/**
+* maximizeTerminal - A function that returns { terminalSize: minContentValue }.
+*
+* @return {Function}
+*/
+const maximizeTerminal = () => ({ terminalSize: minContentValue });
+
+/**
+* split - A function that returns { terminalSize: splitValue }.
+*
+* @return {Function}
+*/
+const split = () => ({ terminalSize: splitValue });
+
+/**
+ * minimizeTerminal - A function that returns { terminalSize: maxContentValue }.
+ *
+ * @return {Function}
+ */
+const minimizeTerminal = () => ({ terminalSize: maxContentValue });
+
+/**
+ * resizeCommands (export) - Returns resize Terminal commands that can be used to
+ * update the Terminal/Content Pane sizes.
+ *
+ * @return {Object}
+ */
+export const resizeCommands = {
+  maximize: maximizeTerminal,
+  max: maximizeTerminal,
+
+  minimize: minimizeTerminal,
+  min: minimizeTerminal,
+
+  split,
+};
